c2b4eaf31d1cee22ba56638a4c069af48abf6f7b
adcerro <adcerro@uninorte.edu.co>
2023-12-22 08:22:57 -0500

    landing page

--- spoticharts/urls.py
+++ spoticharts/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'spoticharts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('charts', views.charts, name='charts'),
 ]
--- spoticharts/views.py
+++ spoticharts/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'spoticharts/index.html')
+
+def about(request):
+    return render(request, 'spoticharts/about.html')
+
+def charts(request):
+    return render(request, 'spoticharts/charts.html')
